12a2503d75e5a1c882085c82da30d70682ebc4ae
barbz77 <abramic7barbara7@gmail.com>
2025-05-09 19:50:18 +0200

    Create udruga za zaštitu životinja.sql

---
+++ udruga za zaštitu životinja.sql
@@ -0,0 +1,21 @@
+﻿--create database udruga;
+
+--use udruga;
+
+--create table osobe(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(100)
+--);
+
+--create table stitenici(
+--sifra int,
+--ime varchar(50),
+--vrsta varchar(50)
+--);
+
+--create table prostori(
+--sifra int,
+--naziv varchar(50),
+--kapacitet int
+--);
