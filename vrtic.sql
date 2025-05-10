8a03ba7c2d2a7e81de1cee5ff7468b2e6bd7d1b0
Barbara Abramić <abramic7barbara7@gmail.com>
2025-05-10 22:54:26 +0200

    Kreiranje tablice vrtic

---
+++ vrtic.sql
@@ -0,0 +1,21 @@
+﻿--create database vrtic
+
+--use vrtic
+
+--create table skupine(
+--sifra int,
+--naziv varchar(50)
+--);
+
+--create table djeca(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(100)
+--);
+
+--create table odgojiteljice(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(100)
+--);
+
