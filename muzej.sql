9f0e4ec32e643dc9494643cc7fb091d1604edce3
Barbara Abramić <abramic7barbara7@gmail.com>
2025-05-12 22:20:30 +0200

    Kreiranje tablice muzej

---
+++ muzej.sql
@@ -0,0 +1,27 @@
+﻿--create database muzej
+
+--use muzej
+
+--create table izlozbe(
+--sifra int,
+--naziv varchar(100),
+--trajanje datetime
+--);
+
+--create table djela(
+--sifra int,
+--naziv varchar(100),
+--autor varchar(100),
+--);
+
+--create table kustosi(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(100)
+--);
+
+--create table sponzori(
+--sifra int,
+--naziv varchar(100),
+--email varchar(100)
+--);
