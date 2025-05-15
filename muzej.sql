4da9fbe461cfcb5afb79acad32d6ea28e1b85ab4
Barbara Abramić <abramic7barbara7@gmail.com>
2025-05-16 00:46:53 +0200

    Kreiranje tablice i dijagrama muzej

--- muzej.sql
+++ muzej.sql
@@ -1,27 +1,31 @@
-﻿--create database muzej
+﻿use master;
+go
+drop database if exists muzej;
+go
+create database muzej
+go
+use muzej
 
---use muzej
+create table izlozbe(
+sifra int not null primary key identity(1,1),
+naziv varchar(100) not null,
+trajanje datetime,
+);
 
---create table izlozbe(
---sifra int,
---naziv varchar(100),
---trajanje datetime
---);
+create table djela(
+sifra int not null primary key identity(1,1),
+naziv varchar(100) not null,
+autor varchar(100) not null,
+izlozba int not null references izlozbe(sifra)
+);
 
---create table djela(
---sifra int,
---naziv varchar(100),
---autor varchar(100),
---);
+create table kustosi(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(100) not null
+);
 
---create table kustosi(
---sifra int,
---ime varchar(50),
---prezime varchar(100)
---);
-
---create table sponzori(
---sifra int,
---naziv varchar(100),
---email varchar(100)
---);
+create table sponzori(
+sifra int not null primary key identity(1,1),
+naziv varchar(100) not null,
+);
