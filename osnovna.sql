39a191b92baaa559e7ebec509fd447dba9395655
Barbara Abramić <abramic7barbara7@gmail.com>
2025-05-16 14:38:45 +0200

    Kreiranje tablice osnovna

---
+++ osnovna.sql
@@ -0,0 +1,31 @@
+﻿--create database osnovna;
+
+--use osnovna;
+
+
+--create table uciteljice(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--predmet varchar(50)
+--);
+
+--create table radionice(
+--sifra int,
+--naziv varchar(50),
+--opis varchar(100),
+--kapacitet int,
+--uciteljica int
+--);
+
+--create table djeca(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--datum_rodjenja datetime
+--);
+
+--create table sudjelovanja(
+--radionica int,
+--dijete int
+--);
