bf9ad7904c6e1e29029d2349495c4ae2fad9dbed
Barbara Abramić <abramic7barbara7@gmail.com>
2025-05-16 14:31:13 +0200

    Kreiranje tablice fakultet

---
+++ fakultet.sql
@@ -0,0 +1,26 @@
+﻿----create database fakultet;
+
+--use fakultet;
+
+--create table studenti(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50)
+--);
+
+--create table rokovi(
+--sifra int,
+--naziv varchar(50),
+--datum_vrijeme datetime
+--);
+
+--create table kolegij(
+--sifra int,
+--naziv varchar(100),
+--ECTS_bodovi int
+--);
+
+--create table prijava_roka(
+--student int,
+--rok int
+--);
