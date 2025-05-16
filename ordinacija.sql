3d89a115b2053f3e6dfcf103024d63057598381d
Barbara Abramić <abramic7barbara7@gmail.com>
2025-05-16 14:20:41 +0200

    Kreiranje tablice i dijagrama ordinacija

---
+++ ordinacija.sql
@@ -0,0 +1,37 @@
+﻿use master;
+go
+drop database if exists ordinacija;
+go
+create database ordinacija;
+go
+use ordinacija;
+
+create table doktori(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+specijalizacija varchar(50) not null
+);
+
+create table pacijenti(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+dat_rodjenja datetime,
+kontakt varchar(50)
+);
+
+create table posjeti(
+sifra int not null primary key identity(1,1),
+datum datetime not null,
+dijagnoza varchar(100) not null,
+terapija varchar(100) not null,
+doktor int not null references doktori(sifra),
+pacijent int not null references pacijenti(sifra)
+);
+
+create table med_sestre(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null
+);
