3ed39c4431c5071c85d8f1e47c634084b46cba12
barbz77 <abramic7barbara7@gmail.com>
2025-06-03 19:52:56 +0200

    UlazIzlaz

---
+++ BACKEND/Ucenje/E01UlazIzlaz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    class E01UlazIzlaz
+    {
+        public static void Izvedi()
+        {
+
+
+            Console.Write("Pozdrav svijetu");
+
+            Console.WriteLine("Isti red");
+
+            Console.WriteLine("Osijek je \n SUPER");
+
+            Console.WriteLine("\t\tOvo je uvuceno");
+
+            Console.Write("Unesi svoje ime: ");
+
+            // varijabla ime
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Unijeli ste {0}", ime);
+           
+
+
+
+        } // kraj metode
+
+
+
+
+
+    } // kraj klase
+} // kraj imenskog prostora(namespace)
