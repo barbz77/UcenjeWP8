0bb7195105c334542629af1115497b0bc909874e
barbz77 <abramic7barbara7@gmail.com>
2025-06-07 09:10:13 +0200

    Promjene

--- BACKEND/Ucenje/Program.cs
+++ BACKEND/Ucenje/Program.cs
@@ -3,7 +3,12 @@
 
 using Ucenje;
 
-E01UlazIzlaz.Izvedi();
+//E05UvjetovanoGrananjeSwitch.Izvodi();
+//E04UvjetovanoGrananjeIf.Izvodi();
+//E03Operatori.Izvodi();
+//E02Z1.Izvodi();
+//E02Varijable.Izvodi();
+//E01UlazIzlaz.Izvodi();
 
 
 
