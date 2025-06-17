1635072337ce2e8ac850847e5e0fa4a2625f9d85
Barbara Abramić <abramic7barbara7@gmail.com>
2025-06-17 19:44:02 +0200

    Završni

---
+++ BACKEND/Ucenje/E14KlasaObjekt/Zavrsni/Igrica.cs
@@ -0,0 +1,12 @@
+﻿namespace Ucenje.E14KlasaObjekt.zavrsni
+{
+    public class Igrica
+    {
+        public int Sifra { get; set; }
+        public string Naziv { get; set; } = "";
+        public decimal Ocjena { get; set; }
+        public DateTime GodinaIzdavanja { get; set; }
+
+
+    }
+}
---
+++ BACKEND/Ucenje/E14KlasaObjekt/Zavrsni/Zanr.cs
@@ -0,0 +1,8 @@
+﻿namespace Ucenje.E14KlasaObjekt.zavrsni
+{
+    public class Zanr
+    {
+        public int Sifra { get; set; }
+        public string Naziv { get; set; } = "";
+    }
+}
